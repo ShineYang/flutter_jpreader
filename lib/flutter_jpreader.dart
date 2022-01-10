ee1f6adcc5177a90bd3e18339d3688f10c689774
kevinzhow <kevinchou.c@gmail.com>
2022-01-10 23:29:27 +0800

    clean(locale): remove redundancy logic

--- lib/flutter_jpreader.dart
+++ lib/flutter_jpreader.dart
@@ -1,5 +1,3 @@
 library flutter_jpreader;
 
 export 'reader_home_page.dart';
-export 'utils.dart';
-export 'supported_language.dart';
--- lib/utils.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter_jpreader/supported_language.dart';
-import 'package:rxdart/rxdart.dart';
-
-class ReaderUtils {
-  static var currentStudyLanguage = BehaviorSubject<ReaderSupportedLanguage>();
-}
